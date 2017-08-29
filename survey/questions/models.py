ddc92be6eb6f599e53b570783632218bd62be3fa
Matt Weems <weemsmatthewj@gmail.com>
2017-08-29 07:53:04 -0500

    shows random question

--- survey/questions/models.py
+++ survey/questions/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    has_voted_on = models.BooleanField(default=False)
 
     def __str__(self):
         return self.question_text
--- survey/questions/views.py
+++ survey/questions/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .models import Question, Choice
+from random import randint
 
 def index(request):
 	question_list = Question.objects.all()
-	context = {'question_list': question_list}
-	return render(request, 'index.html', context)
+	for question in question_list:
+		question.has_voted_on = False
+		question.save()
+	question = question_list[randint(0, question_list.count() - 1)]
+	choices = Choice.objects.filter(question=question)
+	context = {'question': question, 'choices': choices}
+	return render(request, 'detail.html', context)
 
 def detail(request, question_id):
 	question = Question.objects.get(pk=question_id)
